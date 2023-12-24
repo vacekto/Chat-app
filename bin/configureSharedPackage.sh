e95496a00f12921550dd7e9f4f14fe72a42e5052
Tom Vacek <tomas.vacek@avvoka.com>
2023-12-25 00:24:25 +0100

    test

--- bin/configureSharedPackage.sh
+++ bin/configureSharedPackage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE_PATH="$( pwd )/shared/package.json"
+FILE_PATH="$(pwd)/shared/package.json"
 
 if [ "$NODE_ENV" = "production" ]; then
 sed -i 's/.*"main":.*/  "main": "dist\/index.js",/' ${FILE_PATH}
