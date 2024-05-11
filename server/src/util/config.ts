7b7aea1f123c92d4210804052460e273dde973f7
Magic Turtle <vacekto@gmail.com>
2024-05-11 20:20:58 +0200

    fix

--- server/src/util/config.ts
+++ server/src/util/config.ts
@@ -2,4 +2,4 @@ export const APP_URL = "development" ?
     `http://localhost:${process.env.PORT_CLIENT}` :
     `${process.env.VITE_SERVER_URL}`
 export const COOKIE_SAMESITE = "development" ? "none" : "strict"
-export const CORS = APP_URL
+export const CORS = [APP_URL, process.env.PASSKEY_API_URL as string]
